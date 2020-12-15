da0f97df845c5521b3ef73492c4b723209a84f3f
ShubamSinghDadhwal <63088863+ShubamSinghDadhwal@users.noreply.github.com>
2020-12-16 01:30:28 +0530

    Added POST Upload Approval by admin

--- blog/models.py
+++ blog/models.py
@@ -3,15 +3,18 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+
 class Post(models.Model):
     title=models.CharField(max_length=100)
     content=models.TextField()
+    is_verified = models.BooleanField('Is Verified ?', default=False,
+                                                        help_text='Indicates if this post is verified or not?'
+                                                                'For admin users only')
     date_posted=models.DateTimeField(default=timezone.now)
     author=models.ForeignKey(User,on_delete=models.CASCADE)
 
-    def __str__(self):
+    def _str_(self):
         return self.title
     
     def get_absolute_url(self):
         return reverse('post-detail',kwargs={'pk':self.pk})
-
